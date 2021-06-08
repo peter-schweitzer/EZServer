1230b8d47ef417cd113e3391d014c8038a220472
peter-schweitzer <76947241+peter-schweitzer@users.noreply.github.com>
2021-06-08 21:40:42 +0200

    update EZServer.js

--- EZServer.js
+++ EZServer.js
@@ -1,36 +1,76 @@
-let { createServer } = require('http');
-let { readFile } = require('fs');
+const createServer = require('http').createServer;
+const readFile = require('fs').readFile;
 
-let mimeTypes = {
-  html: 'text/html',
-  css: 'text/css',
-  js: 'text/javascript',
-  json: 'application/json',
-  png: 'image/png',
-  /**
-   * @param filePath Path of file
-   */
-  getType: (filePath) => {
-    return mimeTypes[filePath.split('.').pop()];
-  },
-};
-
-let { getType } = mimeTypes;
-
-class App {
+class EZServerApp {
   /**
    * @param {string} port port the server is hosted on
    */
   constructor(port) {
+    this.resolvers = {};
+    this.endoints = [];
+    this.groupResFunctions = {};
+
+    /**
+     * @param {IncomingMessage} req Request from the client
+     * @param {ServerResponse} res Respnose from the server
+     */
+    this.throw404 = (req, res) => {
+      console.log(`404 on "${req.url}"`);
+      this.fetchFromFs('./html/404.html', res);
+    };
+
+    /**
+     * @param {string} reqPath path of requested URL
+     * @param {function} resFunction function to resolve the request
+     */
+    this.addResolver = (reqPath, resFunction) => {
+      this.resolvers[reqPath] = resFunction;
+    };
+
+    /**
+     * @param {String} url pattern of requested URL
+     * @param {function} resFunction function to resolve the request
+     */
+    this.addEndpoint = (url, resFunction) => {
+      this.endpoints.push([url, resFunction]);
+    };
+
+    /**
+     * @param {string} groupName name of the new group
+     * @param {function} resFunction function to resolve the requests
+     */
+    this.createGroup = (groupName, resFunction) => {
+      this.groups[groupName] = resFunction;
+    };
+
+    /**
+     * @param {String} url pattern of requested URL
+     * @param {string} groupName name of the group
+     * */
+    this.addEndpointToGroup = (url, groupName) => {
+      this.addEndpoint(url, this.groupResFunctions[groupName]);
+    };
+
+    /**
+     * @param {IncomingMessage} req request from the client
+     * @returns {function} resFunction function to resolve request
+     */
+    this.getResFromEndpoints = (req) => {
+      for (const endpoint of this.endPoints) {
+        if (req.url.substr(0, endpoint[0].length) == endPoint[0]) return endPoint[1];
+      }
+      return undefined;
+    };
+
     /**
      * @param {string} filePath path of file
      * @param {ServerResponse} res Response from Server
      */
-    this.fetchFS = (filePath, res) => {
+    this.fetchFromFs = (filePath, res) => {
       readFile(filePath, (err, data) => {
         if (err) {
           res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('error while loading file from fs');
+          res.end(`error while loading file from fs:\n${err}`);
           return;
         }
         if (filePath == './html/404.html') {
@@ -38,41 +78,34 @@ class App {
         } else {
           res.writeHead(200, getType(filePath));
         }
-        res.end(data);
+        console.log(`data: "${data}"`);
+        res.write(data);
+        res.end();
       });
     };
 
-    /**
-     * @param {IncomingMessage} req Request from the client
-     * @param {ServerResponse} res Respnose from the server
-     */
-    this.throw404 = (req, res) => {
-      console.log(req.url);
-      fetchFS('./html/404.html', res);
-    };
-
     this.httpServer = createServer((req, res) => {
-      console.log(req.url);
-      (this.resolvers[req.url] || this.endPoints[req.url.split('/')[1]] || this.throw404)(req, res);
-    }).listen(port);
-    this.resolvers = this.endPoints = {};
-
-    /**
-     * @param {string} path path of requested URL
-     * @param {function} resFunction function to resolve the request
-     */
-    this.addResolver = (path, resFunction) => {
-      this.resolvers[path] = resFunction;
-    };
-
-    /**
-     * @param {string} path path of requested URL
-     * @param {function} resFunction function to resolve the request
-     */
-    this.addEndpoint = (path, resFunction) => {
-      this.endPoints[path] = resFunction;
-    };
+      let resolver = this.resolvers[req.url] || this.getResFromEndpoints(req) || this.throw404;
+      resolver(req, res);
+    });
+    this.httpServer.listen(port);
   }
 }
 
-module.exports = { App: App };
+const mimeTypes = {
+  html: 'text/html',
+  css: 'text/css',
+  js: 'text/javascript',
+  json: 'application/json',
+  png: 'image/png',
+};
+
+/**
+ * @param {string} filePath Path of file
+ * @returns {string} mimeType fo the file
+ */
+const getType = (filePath) => {
+  return mimeTypes[filePath.split('.').pop()];
+};
+
+module.exports = { App: EZServerApp };
