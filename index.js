84d2f087aabd0b2712c68a98b933e206c3cf5005
peter-schweitzer <peter.schweitzer.official@gmail.com>
2022-08-31 13:56:46 +0200

    move App to index (prep v2 #1)

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const App = require('./src/App');
-const { buildRes, getType, serveFromFS, getBodyJSON } = require('./src/utils');
+const { createServer, IncomingMessage, ServerResponse } = require('http');
+
+const LOG = console.log;
+const WARN = console.warn;
+
+class App {
+  constructor() {
+    this.m_httpServer = createServer((req, res) => {
+      req.url = decodeURIComponent(req.url);
+      (this.m_restEndpoint(req) || this.m_endpoints[url] || this.m_restRoute(req) || this.m_route(req) || throw404)(req, res);
+    });
+  }
+
+  /** @param {number|string} port port the server is hosted on */
+  listen(port) {
+    this.m_httpServer.listen(port);
+  }
+}
 
 module.exports = { App, buildRes, getType, serveFromFS, getBodyJSON };
 
