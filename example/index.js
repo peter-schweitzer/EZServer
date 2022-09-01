33d4201687dbb121b386a9eeaa69b35362b82f5d
peter-schweitzer <peter.schweitzer.official@gmail.com>
2022-09-01 18:17:28 +0200

    update example/index.js (prep v2 #9)

--- example/index.js
+++ example/index.js
@@ -1,71 +1,81 @@
-const { App, serveFromFS } = require('../index');
+const { App, buildRes, serveFromFS } = require('../');
 
-const app = new App('8080');
+const app = new App();
 
 /**
- ** ================ EZServer Supports Resolvers ===================
+ ** ================ EZServer Supports Endopoints ===================
  */
 
-// - A Resolver resolves a single route.
-// - Resolvers have the highest specificity, so they can over-write Endpoints
+// An Endopoint only resolves the specified URI.
+// Endopoints have the highest specificity, so 'over-write' Routs
 
-app.resolvers.add('/', (req, res) => {
+// these are the most specific, as they only resolve a specific URI with a specific HTTP-mehtods
+app.get('/get', (req, res) => {
+  buildRes(res, 'get');
+});
+app.put('/put', (req, res) => {
+  buildRes(res, 'put');
+});
+app.post('/post', (req, res) => {
+  buildRes(res, 'post');
+});
+app.delete('/delete', (req, res) => {
+  buildRes(res, 'delete');
+});
+app.patch('/patch', (req, res) => {
+  buildRes(res, 'patch');
+});
+
+// add() ignores the HTTP-method, hence it's less specific
+app.add('/', (req, res) => {
   serveFromFS(res, './html/home.html');
 });
 
 /**
- ** ================ EZServer Supports 'regular' Endopoints ===================
+ ** ================ EZServer Supports Routs ===================
  */
 
-// - The only difference to regular Endpoints is that they are filtered by method.
+//These resolve all requests, with the specified HTTP-method, to routes beginning with the specified route ('/' - seperated)
+app.addRestRoute('get', '/rest/get', (req, res) => {
+  buildRes(res, 'get-route');
+});
+app.addRestRoute('put', '/rest/put', (req, res) => {
+  buildRes(res, 'put-route');
+});
+app.addRestRoute('post', '/rest/post', (req, res) => {
+  buildRes(res, 'post-route');
+});
+app.addRestRoute('delete', '/rest/delete', (req, res) => {
+  buildRes(res, 'delete-route');
+});
+app.addRestRoute('patch', '/rest/patch', (req, res) => {
+  buildRes(res, 'patch-route');
+});
 
-//This resolves all routes matching the specified route || uses the most specific endpoint
-app.endpoints.add('/img/', (req, res) => {
-  serveFromFS(res, `.${req.url}`);
+//addRoute again ignores the HTTP-mehtod (similar to add()), hence being less specific than addRestRoute()
+app.addRoute('/route', (req, res) => {
+  buildRes(res, `route`);
 });
 
 /**
- * This creates a group, identified by it's name, containing a [resolv-function]{@link resFunction}.
- * The groups resFunction can be changed on the fly.
- * To do so you can call 'createGroup(groupName, fn)' with the same 'groupName' and a new resFunction 'fn'.
- */
-app.endpoints.createGroup('myGroup', (req, res) => {
-  console.log('myGroup:', req.url);
-});
-
-/**
- * you can add as many routs as you want.
- * they will execute the [resolve-Function]{@link resFunction} of the specified group
- */
-app.endpoints.addToGroup('/endpoint1/', 'myGroup');
-app.endpoints.addToGroup('/endpoint2/', 'myGroup');
-
-/**
- ** ================== EZServer Supports REST Endopoints ======================
+ ** ================ EZServer also has Generic Functions ===================
  */
 
-// - The only difference to regular Endpoints is that they are filtered by method.
-
-//method: 'GET'
-app.rest.get('/api/get', (req, res) => {
-  console.log('rest.get -> req.body:', !!req.body || false);
+// This is an easy way to use the same function multiple times without spamming your own file with functions
+app.addGenericRestFunction('get', 'name', (req, res) => {
+  console.log('rest-name:', req.url);
 });
-//method: 'PSOT'
-app.rest.post('/api/post', (req, res) => {
-  console.log('rest.post -> req.body:', !!req.body || false);
-});
-//method: 'PUT'
-app.rest.put('/api/put', (req, res) => {
-  console.log('rest.put -> req.body:', !!req.body || false);
-});
-//method: 'DELETE'
-app.rest.delete('/api/delete', (req, res) => {
-  console.log('rest.delete -> req.body:', !!req.body || false);
-});
-//method: 'PATCH'
-app.rest.patch('/api/patch', (req, res) => {
-  console.log('rest.patch -> req.body:', !!req.body || false);
+app.addGenericFunction('name', (req, res) => {
+  console.log('name:', req.url);
 });
 
-/** @typedef {import('../src/endpoints/index').resFunction} resFunction */
+//supports endpoints
+app.useGenericRestFunction('get', '/generic/endpoint', 'name');
+app.useGenericFunction('/generic/rest-endpoint', 'name');
+
+//supports routs
+app.useGenericRestFunction('/generic/endpoint', 'name', true);
+app.useGenericFunction('/generic/rest-endpoint', 'name', true);
+
+/** @typedef {import('../index').resFunction} resFunction */
 
