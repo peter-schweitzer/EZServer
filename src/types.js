c7103ea87cd4dbae3c472ba86f9dfc8960899527
peter-schweitzer <peter.schweitzer.official@gmail.com>
2023-05-16 18:58:31 +0200

    mv, refactor & improve types.js

---
+++ src/types.js
@@ -0,0 +1,48 @@
+/**
+ * @typedef {{err: string, data: null}} Err
+ */
+
+/**
+ * @typedef {{err: null, data: T}} Data<T>
+ * @template T
+ */
+
+/**
+ * @typedef {Err|Data<T>} ErrorOr<T>
+ * @template T
+ */
+
+/**
+ * @typedef {Promise<Err|Data<T>>} AsyncErrorOr<T> The Promise should always resolves, never reject!
+ * @template T
+ */
+
+/**
+ * @typedef {false|T} FalseOr<T>
+ * @template T
+ */
+
+/**
+ * @typedef {{[x: string]: T}} LUT<T>
+ * @template T
+ */
+
+/**
+ * @typedef {import('./Parameters').Parameters} Params
+ */
+
+/**
+ * @typedef {IncomingMessage & {uri: string}} EZIncomingMessage
+ */
+
+/**
+ * @typedef {(req: EZIncomingMessage, res: ServerResponse, params: Params) => void} resFunction
+ */
+
+/** @typedef {LUT<resFunction>} resolverLUT */
+
+/**
+ * @typedef {import('http').Server} Server
+ * @typedef {import('http').IncomingMessage} IncomingMessage
+ * @typedef {import('http').ServerResponse} ServerResponse
+ */
