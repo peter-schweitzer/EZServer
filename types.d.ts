8d058ea804e9b867eb423abd9b1c164d7fde9a8a
peter-schweitzer <peter.schweitzer.official@gmail.com>
2023-05-19 00:28:03 +0200

    HOTFIX-RELEASE 3.0.4: fixed exportign from package

--- types.d.ts
+++ types.d.ts
@@ -1,1 +1,4 @@
+export { App } from './src/App.js';
+export { Parameters } from './src/Parameters.js';
 export * from './src/types';
+export { HTTP_METHODS, buildRes, getBodyJSON, serveFromFS, throw404 } from './src/utils.js';
