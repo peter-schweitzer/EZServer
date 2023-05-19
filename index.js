7c36fef9611a729cba2faca4235b2b0b17d39e0a
peter-schweitzer <peter.schweitzer.official@gmail.com>
2023-05-19 10:01:26 +0200

    finally(?) fix type imports/exports

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 export { App } from './src/App.js';
 export { Params } from './src/Params.js';
 export { HTTP_METHODS, buildRes, getBodyJSON, serveFromFS, throw404 } from './src/utils.js';
-export * from './src/index.js';
