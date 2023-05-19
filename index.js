e7ef864be7507a5f0774a223dd199cde7e2ee0fc
peter-schweitzer <peter.schweitzer.official@gmail.com>
2023-05-19 09:31:38 +0200

    improve type exports

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 export { App } from './src/App.js';
-export { Parameters } from './src/Parameters.js';
+export { Params } from './src/Params.js';
 export { HTTP_METHODS, buildRes, getBodyJSON, serveFromFS, throw404 } from './src/utils.js';
-export * from './types.js';
+export * from './src/index.js';
--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,4 @@
 export { App } from './src/App.js';
 export { Parameters } from './src/Parameters.js';
-export * from './src/types';
+export * from './src/index.js';
 export { HTTP_METHODS, buildRes, getBodyJSON, serveFromFS, throw404 } from './src/utils.js';
