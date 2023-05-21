36d375b115543576a4ba7b206a98a489c8814ec6
peter-schweitzer <peter.schweitzer.official@gmail.com>
2023-05-21 20:42:41 +0200

    rm stupid file that fucked me over for far to long

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,2 @@
-export { App } from './src/App.js';
-export * from './src/index.js';
-export { HTTP_METHODS, buildRes, data, err, getBodyJSON, p2eo, serveFromFS, throw404 } from './src/utils.js';
+export * from './index.js';
+export * from './src/types.js';
