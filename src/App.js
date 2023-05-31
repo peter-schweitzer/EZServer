7c62a0db7e91ee9b83ed9c321335cb4b330441fd
peter-schweitzer <peter.schweitzer.official@gmail.com>
2023-05-31 09:57:06 +0200

    creating unneeded objects -> gc -> bad for perf :(

--- src/App.js
+++ src/App.js
@@ -122,7 +122,7 @@ export class App {
     this.m_http_server = createServer((req, res) => {
       const [uri, query_str] = decodeURIComponent(req.url).split('?');
 
-      const ez_incoming_msg = Object.assign({}, req, { uri });
+      const ez_incoming_msg = Object.assign({ uri }, req);
 
       const params_builder = new ParamsBuilder();
       params_builder.add_query_parameters(query_str);
