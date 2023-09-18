peter-schweitzer/EZServer

commit 7c62a0db7e91ee9b83ed9c321335cb4b330441fd

creating unneeded objects -> gc -> bad for perf :(

src/App.js
@@ -122,7 +122,7 @@ export class App {
     this.m_http_server = createServer((req, res) => {
       const [uri, query_str] = decodeURIComponent(req.url).split('?');
 
-      const ez_incoming_msg = Object.assign({}, req, { uri });
+      const ez_incoming_msg = Object.assign({ uri }, req);
 
       const params_builder = new ParamsBuilder();
       params_builder.add_query_parameters(query_str);

commit fb2c4f6c8193bffd0ddb216b6dcd7dc850e47d92

fixed adding 'uri' to 'req' without breaking .on() this used to removed EventEmitter -> getBodyJSON() broken since 7c62a0db

src/App.js
@@ -122,7 +122,7 @@ class App {
     this.m_http_server = createServer((req, res) => {
       const [uri, query_str] = decodeURIComponent(req.url).split('?');
 
-      const ez_incoming_msg = Object.assign({ uri }, req);
+      const ez_incoming_msg = Object.assign(req, { uri });
 
       const params_builder = new ParamsBuilder();
       params_builder.add_query_parameters(query_str);