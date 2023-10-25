88a99b585f7c1310ced17b263905e2b671b7e354
peter-schweitzer <peter.schweitzer.official@gmail.com>
2023-10-25 10:04:50 +0200

    update regex used by sed in test/line-count/run.sh

--- tests/line-count/run.sh
+++ tests/line-count/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd src
-numLines = $(wc -l $(ls) | grep total | sed -nr "s/([0-9]+).*$/\1/p")
-echo $numLines
+numLines=$(wc -l $(ls) | grep total | sed -E "s|(^\W*)([0-9]+)(.*$)|\2|")
 if [[ $(($numLines)) -gt 1000 ]];
 then exit 1
 else exit 0
