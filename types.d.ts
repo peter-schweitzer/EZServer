02899ba05d4afeb9fd23c1faf274ddecf75a7c91
peter-schweitzer <peter.schweitzer.official@gmail.com>
2023-11-25 15:08:12 +0100

    reorganize types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,4 @@
 export { App } from './src/App.js';
-export * from './src/types.js';
 export { buildRes, getBodyJSON, serveFromFS, throw404 } from './src/utils.js';
+
+export * from './src/types.js';
