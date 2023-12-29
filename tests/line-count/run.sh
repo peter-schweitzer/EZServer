df155f82e14b7a40a586867d5ab9380a9bfa8929
peter-schweitzer <peter.schweitzer.official@gmail.com>
2023-12-29 17:46:01 +0100

    improve /test/line-count
    added ANSI color to output of test/line-count.

--- tests/line-count/run.sh
+++ tests/line-count/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
 cd src
-numLines=$(wc -l $(ls) | grep total | sed -E "s|(^\W*)([0-9]+)(.*$)|\2|")
-echo "line count: $numLines"
-if [[ $(($numLines)) -gt 1000 ]];
-then exit 1
-else exit 0
+lineCounts=$(wc -l $(ls))
+
+if [[ $(echo "$lineCounts" | grep total | sed -E "s|^\W*([0-9]+) total$|\1|") -gt 1000 ]];
+then code=1; color="31"
+else code=0; color="32"
 fi
+
+echo "$lineCounts" | sed -E "s|(^\W*)([0-9]+)( total$)|\x1b[$color;1m\1\2/1000\x1b[0m|g"
+exit $code
