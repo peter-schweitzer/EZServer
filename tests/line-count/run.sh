b61f8ce3d1ab85f142dd2f174a6b0e41d89b289c
peter-schweitzer <peter.schweitzer.official@gmail.com>
2023-12-29 17:46:01 +0100

    improve /test/line-count

--- tests/line-count/run.sh
+++ tests/line-count/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd src
-numLines=$(wc -l $(ls) | grep total | sed -E "s|(^\W*)([0-9]+)(.*$)|\2|")
-echo "line count: $numLines"
+echo "" > /dev/tty
+numLines=$(wc -l $(ls) | tee /dev/tty | grep total | sed -E "s|(^\W*)([0-9]+)(.*$)|\2|")
 if [[ $(($numLines)) -gt 1000 ]];
 then exit 1
 else exit 0
