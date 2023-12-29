165b1343e0a1b8f5425cb1dd044ece51f9568ac8
peter-schweitzer <peter.schweitzer.official@gmail.com>
2023-12-29 17:47:31 +0100

    add MIME LUT

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 export { App } from './src/App.js';
-export { buildRes, getBodyJSON, serveFromFS, throw404 } from './src/utils.js';
+export { MIME, buildRes, getBodyJSON, serveFromFS, throw404 } from './src/utils.js';
 
 export * from './src/types.js';
