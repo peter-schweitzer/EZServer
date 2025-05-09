028831ef3dc7c23b23e55d61f3d97ad2b43fb50b
peter-schweitzer <peter.schweitzer.official@gmail.com>
2025-05-09 13:41:30 +0200

    rework test/lien-count script

--- tests/line-count/run.sh
+++ tests/line-count/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 cd src
-lineCounts=$(wc -l $(ls))
 
-if [[ $(echo "$lineCounts" | grep total | sed -E "s|^\W*([0-9]+) total$|\1|") -gt 1000 ]];
-then code=1; color="31"
-else code=0; color="32"
+lineCounts=$(wc -l *)
+cnt=$(echo "$lineCounts" | grep total | sed -E "s| +([0-9]+) total|\1|")
+
+if [[ $cnt -gt 1000 ]]
+then code=1; color=1
+else code=0; color=2
 fi
 
-echo "$lineCounts" | sed -E "s|(^\W*)([0-9]+)( total$)|\x1b[$color;1m\1\2/1000\x1b[0m|g"
+echo "$lineCounts" | sed -E "s|( +)([0-9]+) total$|\1\x1b[9${color};49;1m\2/1000\x1b[0m|g"
 exit $code
