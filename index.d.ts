6cb483199c16c65c90ea4cd4997f1814b860848c
peter-schweitzer <peter.schweitzer.official@gmail.com>
2025-07-27 16:25:17 +0200

    export batteries

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,6 @@ export { App } from './src/App.js';
 export { Params } from './src/Params.js';
 export * from './src/utils.js';
 
+export * as Batteries from './batteries/index.js';
+
 export * from './src/types.js';
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 export { App } from './src/App.js';
 export { Params } from './src/Params.js';
 export * from './src/utils.js';
+
+export * as Batteries from './batteries/index.js';
