b7b9609c0946a6e6c2b92d93c63e87e1a9f28cb4
peter-schweitzer <peter.schweitzer.official@gmail.com>
2026-01-18 22:49:23 +0100

    don't doubly store request head and body in req

--- batteries/Memo/Memo.js
+++ batteries/Memo/Memo.js
@@ -27,18 +27,18 @@ export class Memo {
       return;
     }
 
-    res._head = {};
-    res._body = '';
+    const head_lut = {};
+    let body_parts = [];
 
     res._sh = res.setHeader;
     res.setHeader = (name, value) => {
-      res._head[name] = `${value}`;
+      head_lut[name] = `${value}`;
       return res._sh(name, value);
     };
 
     res._shs = res.setHeaders;
     res.setHeaders = (headers) => {
-      for (const [k, v] of headers) res._head[k] = `${v}`;
+      for (const [k, v] of headers) head_lut[k] = `${v}`;
       return res._shs(headers);
     };
 
@@ -46,15 +46,15 @@ export class Memo {
     res.writeHead = (statusCode, statusMessageOrHeaders, headers) => {
       const head = headers ?? (typeof statusMessageOrHeaders === 'object' ? statusMessageOrHeaders : {});
 
-      for (const k in head) res._head[k] = `${head[k]}`;
+      for (const k in head) head_lut[k] = `${head[k]}`;
 
       return res._wh(statusCode, statusMessageOrHeaders, headers);
     };
 
     res._w = res.write;
     res.write = (chunk, callback) => {
-      if (typeof chunk === 'string') res._body += chunk;
-      else res._body += `${chunk}`;
+      if (typeof chunk === 'string') body_parts.push(chunk);
+      else body_parts.push(`${chunk}`);
 
       return res._w(chunk, callback);
     };
@@ -62,14 +62,14 @@ export class Memo {
     res._e = res.end;
     res.end = (chunk, encoding, cb) => {
       if (chunk !== undefined && typeof chunk !== 'function')
-        if (encoding !== undefined && typeof encoding !== 'function') res._body += chunk.toString(encoding);
-        else res._body += `${chunk}`;
+        if (encoding !== undefined && typeof encoding !== 'function') body_parts.push(chunk.toString(encoding));
+        else body_parts.push(`${chunk}`);
 
       this.#cache[req.url] = {
         status_code: res.statusCode,
         status_message: res.statusMessage,
-        head: res._head,
-        body: res._body,
+        head: head_lut,
+        body: body_parts.join(''),
       };
 
       return res._e(chunk, encoding, cb);
